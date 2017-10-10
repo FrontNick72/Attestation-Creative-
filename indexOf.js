e122b09957f1393eb0b5acbd607bd74dd905bc7c
Демяненко Никита <nd@crtweb.ru>
2017-10-10 14:03:22 +0500

    Change indexOff

--- indexOf.js
+++ indexOf.js
@@ -1,17 +1,23 @@
-if ([].indexOf) { //Кроссбраузерный способ
+let IndexOff = (array, value) => { //1ый способ
+  let index;
 
-  var IndexOff = (array, value) => {  //1ый способ
-    return array.indexOf(value);
+  array.forEach((item, i, arr) => {
+    if (arr[i] === value) index = i;
+  });
+
+  if (index !== undefined) {
+    return index;
+  } else {
+    return -1
   }
+};
 
-} else {
-  var IndexOff = (array, value) => {  //2ой способ
-    for (var i = 0; i < array.length; i++) {
-      if (array[i] === value) return i;
-    }
+// let IndexOff = (array, value) => {  //2ой способ
+//   for (var i = 0; i < array.length; i++) {
+//     if (array[i] === value) return i;
+//   }
 
-    return -1;
-  }
-}
+//   return -1;
+// };
 
-console.log(IndexOff([1, 2, 3], 3));
+console.log(IndexOff([1, 2, 3], 2));
